91cceea58950c72417c3874eda98f3ef306b5669
yashsvi1010 <yashsvisingh97@gmail.com>
2025-05-21 11:09:19 +0530

    add custom hook

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
   useEffect(() => {
     fetch(
       `https://v6.exchangerate-api.com/v6/3a55f25f4f749c387a54af08/latest/${currency}`
-    );
-  }, []);
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
 }
+
+export default useCurrencyInfo;
