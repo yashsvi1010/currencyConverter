230529427234a3bdb6cd358b448821844ab392fc
yashsvi1010 <yashsvisingh97@gmail.com>
2025-05-26 17:52:55 +0530

    added custom hook

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 function InputBox({
   label,
@@ -11,6 +11,7 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -4,12 +4,13 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://v6.exchangerate-api.com/v6/3a55f25f4f749c387a54af08/latest/${currency}`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
+
   console.log(data);
   return data;
 }
